eb6a2448ef9a898f1d5863f08576c4dbb6a6fdbb
Flavio Antunes <flavioantunes457@gmail.com>
2024-12-09 17:49:33 -0300

    incio de teste
    incio de teste

---
+++ inicio.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class inicio : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
